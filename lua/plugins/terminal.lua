e7f66a17c4b4829ed1aa68987f67714ec4bcd028
anuragak021 <anuragak021@gmail.com>
2025-07-31 03:59:45 +0530

    update terminal.lua

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -7,7 +7,6 @@ return {
       local Terminal = require("toggleterm.terminal").Terminal
       local api = vim.api
 
-      -- Setup global plugin options
       toggleterm.setup({
         size = 15,
         hide_numbers = true,
@@ -21,18 +20,15 @@ return {
         shell = vim.o.shell,
       })
 
-      -- Table to hold terminal instances and an index
       local terminals = {}
       local current_index = 0
 
-      -- Function to open a new horizontal terminal
       local function new_terminal()
         local term = Terminal:new({ direction = "horizontal" })
         table.insert(terminals, term)
         return term, #terminals
       end
 
-      -- Helper to focus or open terminal window
       local function focus_term(term)
         local win = term.window
         if win and api.nvim_win_is_valid(win) then
@@ -42,7 +38,6 @@ return {
         end
       end
 
-      -- Open or focus terminal by index
       local function open_term_by_index(i)
         local term = terminals[i]
         if term then
@@ -54,7 +49,6 @@ return {
         end
       end
 
-      -- Standard keymaps
       vim.keymap.set({"n", "t"}, "<C-`>", function()
         local term, idx = new_terminal()
         current_index = idx
@@ -73,18 +67,13 @@ return {
         focus_term(terminals[current_index])
       end, { desc = "Previous Terminal" })
 
-      -- Exit terminal mode
       vim.keymap.set('t', '<C-t>', [[<C-\><C-n>]], { desc = "Terminal to Normal Mode" })
-      -- Directly switch from terminal back to editor window
-      -- Exit to normal mode and move cursor up to the editor split
       vim.keymap.set('t', '<C-e>', [[<C-\><C-n><C-w>k]], { desc = "Go to Editor Window" })
 
-      -- Toggle all terminals
       vim.keymap.set({"n", "t"}, "<C-b>", function()
         vim.cmd("ToggleTermToggleAll")
       end, { desc = "Toggle All Terminals" })
 
-      -- Map <leader>1..9 to open (not toggle) specific terminal
       for i = 1, 9 do
         vim.keymap.set('n', '<leader>' .. i, function()
           open_term_by_index(i)
