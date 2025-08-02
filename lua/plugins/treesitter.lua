ef27b141d9536de5c9a44653a66c180e16f69ab2
anuragak021 <anuragak021@gmail.com>
2025-08-03 04:41:20 +0530

    Update markdown plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local config = require("nvim-treesitter.configs")
 		config.setup({
 			auto_install = true,
-			ensure_installed = { "lua", "javascript", "cpp" },
+			ensure_installed = { "lua", "javascript", "cpp" , "vim" , "c" , "vimdoc" , "query","html" , "markdown" , "markdown_inline"},
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
